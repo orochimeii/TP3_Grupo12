ea501b80595c3425ac7a331a5bf6e04e3c3394c3
orochimeii <babymeyfong@gmail.com>
2022-04-20 22:32:49 -0300

    ejercicio 1 completo

--- TP3_Grupo12/src/main/java/ar/edu/unju/fi/ejercicio1/Principal.java
+++ TP3_Grupo12/src/main/java/ar/edu/unju/fi/ejercicio1/Principal.java
@@ -3,18 +3,51 @@ package ar.edu.unju.fi.ejercicio1;
 import java.util.Scanner;
 
 public class Principal {
+	
+//	 declare dos
+//	 arreglos que almacenen elementos de tipo enteros. Utilizando un objeto scanner solicite al usuario que
+//	 ingrese por consola 5 números que serán guardados en el primer arreglo, luego solicite otros 5 números que
+//	 serán guardados en el segundo arreglo.
+//	 Construya un método que muestre por consola la división entre los números del primer arreglo y el segundo
+//	 arreglo, los índices deben corresponderse al momento de hacer la división.
+//	 Agregue el manejo de excepciones si fuera necesario.
 
+	public static int[] numeros = new int[5];
+	public static int[] numeros2 = new int[5];
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//int[] numeros;
-		int[] numeros2 = new int[5];
 		Scanner scanner = new Scanner(System.in);
-		for(int i = 0; i < numeros2.length; i++) {
+		
+		for(int i = 0; i < numeros.length; i++) {
 			System.out.println("Ingrese un numero");
 			int n = scanner.nextInt();
-			numeros2[i] = n;
+			numeros[i] = n;
+			if (i == numeros.length - 1) {
+				System.out.println("***Primer array completo");
+			}
+		}
+		
+		for(int j = 0; j < numeros2.length; j++) {
+			System.out.println("Ingrese un numero");
+			int n = scanner.nextInt();
+			numeros2[j] = n;
+			if (j == numeros2.length - 1) {
+				System.out.println("***Segundo array completo");
+			}
 		}
 		scanner.close();
+		try {
+			dividirArrays(numeros, numeros2);
+		}catch(ArithmeticException ae) {
+			System.out.println("no me es posible calcular el resultado de una division por 0");
+		}
+	}
+	public static int dividirArrays(int[] arr1, int[] arr2) {
+		for(int k = 0; k < numeros.length; k++) {
+			System.out.println(numeros[k] / numeros2[k]);
+		}
+		return 0;
 	}
 
 }
